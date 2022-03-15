9fd90c378037daf2c68db028b448c1bf950fc6c0
cozynye <cozynye@gmail.com>
2022-03-15 22:23:12 +0900

    Docs : 20220315TIL add

--- test.js
+++ test.js
@@ -1,32 +1,13 @@
-var one = 1;
+const number = 123;
+const string = 'Test';
+const boolean = true;
+const Undefined = undefined;
+const Null = null;
+const symbol = Symbol('symbol');
 
-console.log(prev) // undefined 자바스크립트 호이스팅 특성
-
-function change(){
-  console.log(one);
-
-  let two = 2;
-  const three = 3;
-
-  
-
-  console.log(two);
-  console.log(three);
-
-  one = 11;
-  two = 22;
-  // three=33;  Uncaught TypeError: Assignment to constant variable.
-}
-var prev=3
-change();
-
-console.log(one); // 11
-
-var one = '일입니다'
-
-console.log(one); // 일입니다
-
-// console.log(two); //two is not defined at
-
-const three = 33;
-console.log(three); //33 
+console.log(typeof number); // number
+console.log(typeof string); // string
+console.log(typeof boolean); // boolean
+console.log(typeof Undefined); // undefined
+console.log(typeof Null); // object
+console.log(typeof symbol); // symbol
